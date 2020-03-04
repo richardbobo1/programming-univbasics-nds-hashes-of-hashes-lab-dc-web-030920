f720961b972be690bcd5de09fa4119f21cb88b40
Richard Bobo <richard.bobo@gmail.com>
2020-03-04 20:39:58 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,6 +31,30 @@
 # }
 
 def naming_system
+  
+  starting_node = {
+    
+   :label => "Kingdom",
+   :sub_category => {
+    :label =>"Phylum",
+     :sub_category => {
+       :label => "Class", 
+       :sub_category => {
+         :label => "Order",
+           :sub_category => {
+              :label => "Family", 
+              :sub_category => {
+              :label => "Genus",
+                 :sub_category => {
+                 :label => "Species"}
+         
+         
+       }}}}}
+    
+    
+    
+  }
+  
   # Remember:
   #  Kingdom
   #  Phylum
